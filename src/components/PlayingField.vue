a0a4d79edeb3277d490aa7f30c85240153e26697
albina25 <axioma,25@gmail.com>
2022-11-28 21:36:27 +0500

    refactor: delete FieldCell

--- src/components/PlayingField.vue
+++ src/components/PlayingField.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="playing-field">
-    <div>
-      <p>Рануд: {{ round }}</p>
-    </div>
+    <audio src="../assets/sound/beep.mp3" id="audio"></audio>
+    <p>Раунд: {{ formatedRound }}</p>
     <div class="field">
-      <field-cell
+      <div
         v-for="cell of cells"
-        :cell="cell"
         :key="cell.id"
-        @select-by-user="selectByUser"
-      >
-      </field-cell>
+        :class="[
+          'cell',
+          cell.sequence > 0 && cell.randomSelected === true
+            ? 'filed-cell-active'
+            : '',
+        ]"
+        @click="selectByUser(cell.id)"
+      ></div>
     </div>
     <div class="action">
       <p>{{ textStage }}</p>
@@ -58,13 +61,9 @@
 </template>
 
 <script>
-import FieldCell from "@/components/FieldCell";
-
 export default {
   name: "PlayingField",
-  components: {
-    FieldCell,
-  },
+
   data() {
     return {
       round: 0,
@@ -81,6 +80,9 @@ export default {
     this.createPlayingField();
   },
   computed: {
+    formatedRound() {
+      return this.round ? this.round : "-";
+    },
     textStage() {
       if (this.playing && this.round === this.sequence.length) {
         return "Повторите последовательность.";
@@ -151,6 +153,9 @@ export default {
       if (!this.playing) {
         return;
       }
+      let audio = document.getElementById("audio");
+      audio.currentTime = 0;
+      audio.play();
       this.selected.push(id);
       this.checkResult();
     },
@@ -180,13 +185,13 @@ export default {
 
 <style scoped lang="scss">
 .playing-field {
-  width: 500px;
   margin: 20px auto;
 }
 .field {
   display: flex;
   flex-wrap: wrap;
   width: 180px;
+  height: 180px;
   background: var(--gray-60white);
   margin: 20px auto;
 }
@@ -211,8 +216,38 @@ export default {
 
 .btn-difficult {
   margin-right: 15px;
+
   &--active {
     background-color: var(--green);
+  }
+}
+
+.cell {
+  width: 70px;
+  height: 70px;
+  background: var(--gray-20white);
+  margin: 10px;
+  display: inline-block;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+.filed-cell-active {
+  animation-duration: 1s;
+  animation-fill-mode: both;
+  animation-name: pulse;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale3d(1, 1, 1);
+  }
+  50% {
+    transform: scale3d(1.05, 1.05, 1.05);
+  }
+  100% {
+    background-color: var(--green);
+    transform: scale3d(1, 1, 1);
   }
 }
 </style>
